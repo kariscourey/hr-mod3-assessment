ff5e2a5c21864774c38738b31c05ca56680fe2f2
karis courey <kariscourey@gmail.com>
2022-12-07 11:23:38 -0500

    update

--- books.sql
+++ books.sql
@@ -13,6 +13,14 @@ WHERE co.returned_on IS NULL
 2 2  Preparing Inst   Bob    2022-09-05
 4 1  Make: Elect      Grace  2022-09-01
 
+-- soln
+SELECT b.title, c.checked_out_on
+FROM book b
+INNER JOIN checkout c ON c.book_id = b.id
+INNER JOIN member m on m.id = c.member_id
+WHERE c.checkout_out_on IS NOT NULL AND returned_on IS NULL
+
+
 
 -- Write your query for "complete book histories"
 SELECT b.title as title,
@@ -25,3 +33,10 @@ LEFT OUTER JOIN checkout co
   ON (b.id = co.book_id)
 LEFT OUTER JOIN member m
   ON (co.member_id = m.id)
+
+
+-- soln
+SELECT b.title, b.copies, c.checked_out_on, c.returned_on, m.name
+FROM book b
+LEFT JOIN checkout c ON c.book_id = b.id
+LEFT JOIN member m on m.id = c.member_id
