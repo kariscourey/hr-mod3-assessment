339374388a4a8ac4dae1ddf5ee955d0a0a89032a
karis courey <kariscourey@gmail.com>
2022-12-07 10:16:27 -0500

    update with ll soln

--- linked-list.py
+++ linked-list.py
@@ -11,11 +11,18 @@ linked_list = n1
 
 def last_entry(linked_list):
 
+    # if linked_list:
+    #     current_node = linked_list
+    #     while current_node.link is not None:
+    #         current_node = current_node.link
+    #     return current_node
+    # return None
+
     if linked_list:
-        current_node = linked_list
-        while current_node.link is not None:
-            current_node = current_node.link
-        return current_node
-    return None
+        node = linked_list
+        while True:
+            if node.link is None:
+                return node
+            node = node.link
 
 print(last_entry(linked_list))
